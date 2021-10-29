aabc304e1cffce89521dde9ff19d473c3ada9280
Jeff Rogers <jeff.rogers@spire.com>
2021-10-29 00:29:33 +0000

    ADCS/TFRS manager/cache

---
+++ agent/server/impl/Adcs.h
@@ -0,0 +1,27 @@
+/**
+ * Adcs.h
+ * 
+ * Adcs and Tfrs conversion and caching utility.
+ * 
+ * Copyright (c) 2021 Spire Global, Inc.
+ */
+#pragma once
+
+#include <chrono>  // NOLINT(build/c++11)
+
+#include <ussp/payload/PayloadAdcsFeed.hpp>
+#include <ussp/tfrs/ReceiverNavigationState.hpp>
+#include "AdcsResponse.h"
+#include "TfrsResponse.h"
+
+class AdcsManager {
+    org::openapitools::server::model::AdcsResponse m_adcs;
+    std::chrono::time_point<std::chrono::system_clock> m_adcs_mtime;
+    org::openapitools::server::model::TfrsResponse m_tfrs;
+    std::chrono::time_point<std::chrono::system_clock> m_tfrs_mtime;
+ public:
+    void setAdcs(const ussp::payload::PayloadAdcsFeed& adcs);
+    void setTfrs(const ussp::tfrs::ReceiverNavigationState& tfrs);
+    org::openapitools::server::model::AdcsResponse getAdcs();
+    org::openapitools::server::model::TfrsResponse getTfrs();
+};
